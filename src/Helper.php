9561d686c1cc943ede79869dccd3afae28d4d7f9
quanangel <quanangel@outlook.com>
2020-12-21 02:58:07 +0800

    add-Helper class(test)

---
+++ src/Helper.php
@@ -0,0 +1,12 @@
+<?php
+declare (strict_types = 1);
+
+/**
+ * Helper function class
+ */
+
+if (!function_exists("test")) {
+    function test() {
+        echo "is Test Helper Function";
+    }
+}
